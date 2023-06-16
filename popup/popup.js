363f04f1f059a78c171c469405af1ebce6c98cab
Varinder Singh <varindersingh271990@gmail.com>
2023-06-16 11:40:09 +0530

    updated button styling

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,5 @@
+const DARK_MODE = "Dark Mode";
+const LIGHT_MODE = "Light Mode";
 const darkModeBtn = document.getElementById("dark-mode");
 
 const getActiveTabId = async () => {
@@ -10,38 +12,53 @@ const getActiveTabId = async () => {
     }
 }
 
-darkModeBtn.addEventListener("click", async () => {
-    const data = await new Promise((resolve) => chrome.storage.local.get("darkMode", (result) => resolve(result)));
+const switchBtn = (mode) => {
+    if(mode === DARK_MODE){
+        darkModeBtn.classList.remove("light");
+        darkModeBtn.classList.add("dark");
+    }else if(mode === LIGHT_MODE){
+        darkModeBtn.classList.remove("dark");
+        darkModeBtn.classList.add("light");
+    }
+}
+
+
+const switchMode = async (mode) => {
     const my_tabid = await getActiveTabId();
-    if (data.darkMode) {
-      await new Promise((resolve) => chrome.storage.local.set({ darkMode: false }, () => resolve()));
-      darkModeBtn.innerText = "Dark Mode";
-      chrome.scripting.executeScript({
-        target: { tabId: my_tabid },
-        function: () => {
+    let darkMode = false;
+    if(mode === DARK_MODE) {
+        darkMode = true;
+    }else if(mode === LIGHT_MODE){
+        darkMode = false;
+    }
+    await new Promise((resolve) => chrome.storage.local.set({ darkMode }, () => resolve()));
+    switchBtn();//Update button style
+    darkModeBtn.innerText = mode;
+    chrome.scripting.executeScript({
+      target: { tabId: my_tabid },
+      function: (mode,DARK_MODE,LIGHT_MODE) => {
+        if(mode === DARK_MODE){
             document.body.classList.remove("dark-mode");
             document.body.classList.add("light-mode");
-        }
-      });
-      chrome.scripting.insertCSS({
-        target: { tabId: my_tabid },
-        files: ["content.css"], // Replace with the appropriate CSS file name
-      });
-      
-    } else {
-      await new Promise((resolve) => chrome.storage.local.set({ darkMode: true }, () => resolve()));
-      darkModeBtn.innerText = "Light Mode";
-      chrome.scripting.executeScript({
-        target: { tabId: my_tabid },
-        function: () => {
+        }else if(mode === LIGHT_MODE){
             document.body.classList.remove("light-mode");
             document.body.classList.add("dark-mode");
         }
-      });
-      chrome.scripting.insertCSS({
-        target: { tabId: my_tabid },
-        files: ["content.css"], // Replace with the appropriate CSS file name
-      });
+      },
+        args: [mode,DARK_MODE,LIGHT_MODE]
+    });
+    chrome.scripting.insertCSS({
+      target: { tabId: my_tabid },
+      files: ["content.css"]
+    });
+};
+
+darkModeBtn.addEventListener("click", async () => {
+    const data = await new Promise((resolve) => chrome.storage.local.get("darkMode", (result) => resolve(result)));
+    if (data.darkMode) {
+        switchMode(LIGHT_MODE);
+    } else {
+        switchMode(DARK_MODE);
     }
   });
   
